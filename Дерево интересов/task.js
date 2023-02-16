683c07946d0ceb5152c317fa0b6731d0dec2510f
Vasilkova Lyubov <damirana_@mail.ru>
2023-02-17 01:47:51 +0700

    increased level of difficulty

--- Дерево интересов/task.js
+++ Дерево интересов/task.js
@@ -2,6 +2,7 @@ const elements = document.querySelectorAll('.interests.interests_active');
 
 for (let elem of elements) {
     const childrens = elem.querySelectorAll('input')
+    const childrensArr = Array.from(childrens)
     const parent = elem.previousElementSibling.querySelector('input')
 
     parent.onchange = () => {
@@ -16,4 +17,26 @@ for (let elem of elements) {
         }
       }
     }
+
+  for (let child of childrens) { 
+    
+    child.onchange = () => {
+      if (childrensArr.some(isBigEnough) && (!childrensArr.every(isBigEnough))) {
+        parent.indeterminate = true
+      }
+      else if (childrensArr.every(isBigEnough)) {
+        parent.indeterminate = false
+        parent.checked = true
+      }
+      else {
+        parent.indeterminate = false
+        parent.checked = false
+      }
+    }
+
   }
+  }
+
+function isBigEnough(element) {
+  return element.checked
+}
