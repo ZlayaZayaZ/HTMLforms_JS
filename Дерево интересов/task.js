16be571aa4cebca2be717c2d4f1adb34bced2173
Vasilkova Lyubov <damirana_@mail.ru>
2023-02-22 18:58:51 +0700

    task intermediate result

--- Дерево интересов/task.js
+++ Дерево интересов/task.js
@@ -21,21 +21,57 @@ for (let elem of elements) {
   for (let child of childrens) { 
     
     child.onchange = () => {
-      if (childrensArr.some(isBigEnough) && (!childrensArr.every(isBigEnough))) {
-        parent.indeterminate = true
+      while (examinationParent (child)) {
+        let siblingsArr, parent = createArr(child)
+        examination(siblingsArr, parent)
+        child = parent
       }
-      else if (childrensArr.every(isBigEnough)) {
-        parent.indeterminate = false
-        parent.checked = true
-      }
-      else {
-        parent.indeterminate = false
-        parent.checked = false
-      }
-    }
+    } 
+  }
+  
+function examinationParent (child) {
+  const ul = child.closest('ul')
+  const parent = ul.previousElementSibling.querySelector('input')
+  return parent
+}
 
+function createArr (child) {
+  let siblings = []
+  let parent = examinationParent (child)
+    // childrensArr2.push(child)
+    let li = child.closest('li')
+    let next = li.nextElementSibling
+    while (next != null) {
+      let input = next.querySelector('input')
+      siblings.push(input)
+      li = input.closest('li')
+      next = li.nextElementSibling
+    }
+    let previous = li.previousElementSibling
+    while (previous != null) {
+      let input = previous.querySelector('input')
+      siblings.push(input)
+      li = input.closest('li')
+      previous = li.previousElementSibling
+    }
+    let siblingsArr = Array.from(siblings)
+    return siblingsArr, parent
+}
+
+function examination (childrensArr, parent) {
+  if (childrensArr.some(isBigEnough) && (!childrensArr.every(isBigEnough))) {
+    parent.indeterminate = true
   }
+  else if (childrensArr.every(isBigEnough)) {
+    parent.indeterminate = false
+    parent.checked = true
   }
+  else {
+    parent.indeterminate = false
+    parent.checked = false
+  }
+}
+}
 
 function isBigEnough(element) {
   return element.checked
